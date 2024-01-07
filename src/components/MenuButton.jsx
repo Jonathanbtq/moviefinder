95750d56712d46fbfa44290faa38c61ba04d33db
Jonathanbtq <botquin.jonathan@yahoo.fr>
2024-01-07 02:13:30 +0100

    randomadd/movie

--- src/components/MenuButton.jsx
+++ src/components/MenuButton.jsx
@@ -1,57 +1,116 @@
 export default function MenuButton({ updateMovie }){
 
+    const apiBaseUrl = 'https://api.themoviedb.org/3/movie';
+
+    const options = {
+        method: 'GET',
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTFjZWM2NWFmMDRmOWY0Njk5M2UxM2RjOWRlZDFiNiIsInN1YiI6IjY1OTk5ZGQ4ODliNTYxMDA5NDhiODJiMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oAD2ebs7sBFTF0wEGVVwUNd1uoN-nynXJJOqNnXFwYE'
+        }
+    };
+
+    // const fetchData = (endpoint) => {
+    //     return fetch(`${apiBaseUrl}/${endpoint}`, options)
+    //         .then(response => response.json())
+    //         .catch(error => {
+    //             console.error(error);
+    //             throw error;
+    //         });
+    // }
+
+    // const getRandomMovieId = (response) => {
+    //     if (response.length === 0) {
+    //         return null
+    //     }
+
+    //     const newMovieIds = response.map(movie => movie.id)
+    //     const randomIndex = Math.floor(Math.random() * newMovieIds.length)
+    //     return newMovieIds[randomIndex]
+    // }
+
+    // const changeMovieButton = (state) => {
+    //     switch (state) {
+    //         case 'toprated':
+    //         case 'endiffusion':
+    //         case 'avenir':
+    //         case 'hasard':
+    //             fetchData(`${state}?language=fr&page=1&region=fr`);
+    //             break;
+    //         default:
+    //             break;
+    //     }
+    // };
+
+    // const handleRandomMovie = () => {
+    //     fetchData('popular?language=fr&page=1&region=fr')
+    //         .then(response => {
+    //             console.log(response);
+    //             const randomMovieId = getRandomMovieId(response);
+    
+    //             if (randomMovieId) {
+    //                 return fetchData(`${randomMovieId}?language=fr`);
+    //             }
+    //         })
+    //         .then(data => {
+    //             if (data) {
+    //                 updateMovie(data);
+    //             }
+    //         })
+    //         .catch(err => {
+    //             console.error(err);
+    //         });
+    // };
+
     const changeMovieButton = (state) => {
-        const options = {
-            method: 'GET',
-            headers: {
-              accept: 'application/json',
-              Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTFjZWM2NWFmMDRmOWY0Njk5M2UxM2RjOWRlZDFiNiIsInN1YiI6IjY1OTk5ZGQ4ODliNTYxMDA5NDhiODJiMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.oAD2ebs7sBFTF0wEGVVwUNd1uoN-nynXJJOqNnXFwYE'
-            }
-        };
 
         const randomMovie = (response) => {
             if (response.length === 0) {
                 return null;
             }
-            const random = Math.floor(Math.random(response)* response.length)
-            return random
+
+            const newMovieId = []
+            response.forEach(movie => {
+                newMovieId.push(movie.id)
+            });
+            const random = Math.floor(Math.random()* newMovieId.length)
+            const randomId = newMovieId[random];
+            return randomId
         }
 
         
-        if (state === 'avenir') {    
-              fetch('https://api.themoviedb.org/3/movie/upcoming?language=fr&page=1&region=fr', options)
+        if (state === 'upcoming') {    
+              fetch(`${apiBaseUrl}/upcoming?language=fr&page=1&region=fr`, options)
                 .then(response => response.json())
                 .then(response => {
                     updateMovie(response.results)
                 })
                 .catch(err => console.error(err));
-        } else if (state === 'toprated') {
-            fetch('https://api.themoviedb.org/3/movie/top_rated?language=fr&page=1&region=fr', options)
+        } else if (state === 'top_rated') {
+            fetch(`${apiBaseUrl}/top_rated?language=fr&page=1&region=fr`, options)
                 .then(response => response.json())
                 .then(response => {
                     updateMovie(response.results)
                 })
                 .catch(err => console.error(err));
-        } else if (state === 'endiffusion') {
-              fetch('https://api.themoviedb.org/3/movie/now_playing??language=fr&page=1&region=fr', options)
+        } else if (state === 'now_playing') {
+              fetch(`${apiBaseUrl}/now_playing??language=fr&page=1&region=fr`, options)
                 .then(response => response.json())
                 .then(response => {
                     updateMovie(response.results)
                 })
                 .catch(err => console.error(err));
         } else if (state === 'hasard') {
-            fetch('https://api.themoviedb.org/3/movie/popular?language=fr&page=1&region=fr', options)
+            fetch(`${apiBaseUrl}/popular?language=fr&page=1&region=fr`, options)
                 .then(response => response.json())
                 .then(response => {
-                    const randomData = randomMovie(response.results)
-                    console.log(randomData)
-                    if (randomData === 0){
-                        randomData = randomData + 1
-                        if (response.results.id === randomData) {
-                            updateMovie(response.results)
-                        }
-                    }
-                    
+                    let randomData = randomMovie(response.results)
+                    fetch(`https://api.themoviedb.org/3/movie/${randomData}?language=en-US`, options)
+                        .then(response => response.json())
+                        .then(response => {
+                            updateMovie(response)
+                        })
+                        .catch(err => console.error(err));
                 })
                 .catch(err => console.error(err));
         }
@@ -59,13 +118,13 @@ export default function MenuButton({ updateMovie }){
     return (
         <>
             <div className="button_div">
-                <button onClick={() => changeMovieButton('toprated')}>
+                <button onClick={() => changeMovieButton('top_rated')}>
                     <p>Top Rated</p>
                 </button>
-                <button onClick={() => changeMovieButton('endiffusion')}>
+                <button onClick={() => changeMovieButton('now_playing')}>
                     <p>En diffusion</p>
                 </button>
-                <button onClick={() => changeMovieButton('avenir')}>
+                <button onClick={() => changeMovieButton('upcoming')}>
                     <p>A venir</p>
                 </button>
                 <button onClick={() => changeMovieButton('hasard')}>
